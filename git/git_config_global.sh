b9c1f45762b13b346fef2a632c206788f63c6d4c
Peter Shilliday <pbshilli@users.noreply.github.com>
2019-03-07 17:16:34 -0600

    Update/fix Git configuration

--- git/git_config_global.sh
+++ git/git_config_global.sh
@@ -6,12 +6,13 @@ git config --global user.email pbshilli@users.noreply.github.com
 git config --global core.editor vim
 git config --global color.interactive.prompt "bold white"
 git config --global rebase.stat true
+git config --global push.default current
 git config --global diff.context 7
 git config --global status.showuntrackedfiles all
 
 # Global gitignore
 git config --global core.excludesfile ~/.gitignore
-echo "*.swp" > ~./gitignore
+echo "*.swp" > ~/.gitignore
 
 # Common git aliases
 git config --global alias.ci "commit"
