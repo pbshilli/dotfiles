1bc2179dfc119c1aac91415775116601971fe570
Peter Shilliday <pbshilli@users.noreply.github.com>
2023-04-18 20:18:46 -0500

    git: Add cmd to config-diff-cli.sh

--- git/config-diff-cli.sh
+++ git/config-diff-cli.sh
@@ -3,3 +3,5 @@ git config --global diff.tool nvimdiff
 git config --global merge.tool nvimdiff
 git config --global merge.conflictstyle diff3
 
+# For git instances that don't recognize nvimdiff:
+# git config --global difftool.nvimdiff.cmd 'nvim -d $LOCAL $REMOTE'
