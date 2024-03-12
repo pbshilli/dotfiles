2c6b437175c3d70b7014f0e9bd3e3de2c375e75f
Peter Shilliday <peter.shilliday@garmin.com>
2024-03-12 13:10:20 -0500

    git: Add commit.verbose=true

--- git/config-basic.sh
+++ git/config-basic.sh
@@ -4,4 +4,5 @@ git config --global rebase.stat true
 git config --global push.default current
 git config --global diff.context 7
 git config --global status.showuntrackedfiles all
+git config --global commit.verbose true
 
