1cf194a72ecbfde9ac2f33d3cadf5ac9f1e7cfc5
Peter Shilliday <Peter.Shilliday@garmin.com>
2024-12-10 09:48:44 -0600

    git: Improve config setup scripts

--- git/config-alias.sh
+++ git/config-alias.sh
@@ -1,14 +1,13 @@
 # Common git aliases
-git config --global alias.ci "commit"
-git config --global alias.co "checkout"
-git config --global alias.df "diff"
-git config --global alias.ds "diff --stat"
-git config --global alias.dt "difftool"
-git config --global alias.lol "log --graph --decorate --pretty=oneline --abbrev-commit"
-git config --global alias.lola "log --graph --decorate --pretty=oneline --abbrev-commit --all"
-git config --global alias.mt "mergetool"
-git config --global alias.rou "config remote.origin.url"
-git config --global alias.st "status"
-git config --global alias.su "submodule update"
-git config --global alias.nuke "!git reset --hard && git submodule update --init --recursive && git clean -dfx -e*.swp"
-
+git config --global alias.ci 'commit'
+git config --global alias.co 'checkout'
+git config --global alias.df 'diff'
+git config --global alias.ds 'diff --stat'
+git config --global alias.dt 'difftool'
+git config --global alias.lol 'log --graph --decorate --pretty=oneline --abbrev-commit'
+git config --global alias.lola 'log --graph --decorate --pretty=oneline --abbrev-commit --all'
+git config --global alias.mt 'mergetool'
+git config --global alias.rou 'config remote.origin.url'
+git config --global alias.st 'status'
+git config --global alias.su 'submodule update'
+git config --global alias.nuke '!git clean -dfx && git submodule foreach --recursive git clean -dfx && git reset --hard && git submodule foreach --recursive git reset --hard && git pull --prune && git submodule update --init --recursive && git gc'
--- git/config-basic.sh
+++ git/config-basic.sh
@@ -1,8 +1,13 @@
 # Basic preferences
-git config --global color.interactive.prompt "bold white"
+git config --global color.interactive.prompt 'bold white'
 git config --global rebase.stat true
+
+# Push to the branch of the same name by default
 git config --global push.default current
 git config --global diff.context 7
 git config --global status.showuntrackedfiles all
 git config --global commit.verbose true
+
+# When perfoming garbage collection, nuke all pruned branches
+git config --global gc.pruneExpire now
 
